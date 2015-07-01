1bed8135473980744c19929dfbdd0cf1e4a5956d
danielzengqx <cheer_zeng@163.com>
2015-07-02 03:25:49 +0800

    modify ip address

--- joins/views.py
+++ joins/views.py
@@ -175,12 +175,12 @@ def weixin(request):
 		    		vl_title1 = "AA活动助手"
 		    		vl_description1 = "AA活动助手"
 		    		vl_pic_url1 = "http://b87.photo.store.qq.com/psb?/V117jtH91i6nzd/*iBbJ98RLScbg*EF4QwUsi3rYA1zWHBJYq*hw6qM3a4!/b/dODm6DPGfAAA&bo=ngK*AQAAAAABAAU!&rf=viewer_4&t=5"
-                                vl_url1 = "http://117.78.5.184/huodong/"	
+                                vl_url1 = "http://120.25.163.62/huodong/"	
 
 		    		vl_title = "发布活动"
 		    		vl_description = "发布活动"
 		    		vl_pic_url = "http://b88.photo.store.qq.com/psb?/V117jtH91i6nzd/a2xngiBE0QvjwOHbXEi4kltiOhcn59l1Qm9pgpuR*pA!/b/dLpIdTT5SAAA&bo=ngK.AQAAAAABAAQ!&rf=viewer_4&t=5"
-                                vl_url = "http://117.78.5.184/huodong/"										
+                                vl_url = "http://120.25.163.62/huodong/"										
                                 
                                 response = "<xml>\
                                 <ToUserName><![CDATA[" + fromUser + "]]></ToUserName>\
